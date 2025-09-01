06bac9c18be1d3bbf6b120f0258ce034ab5c009d
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-09-01 23:14:05 +0530

    First Problem Commit

---
+++ 800 Rating/A_Halloumi_Boxes.java
@@ -0,0 +1,37 @@
+
+
+import java.util.*;
+
+public class A_Halloumi_Boxes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int noe = sc.nextInt();
+
+        while(noe != 0)
+        {
+            int size = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] arr = new int[size];
+
+            for(int i = 0 ; i < size ; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+
+            int[] copy = Arrays.copyOf(arr,size);
+            Arrays.sort(arr);
+
+            if(Arrays.equals(arr, copy)|| k > 1)
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            noe--;
+        }
+        
+    }
+}
