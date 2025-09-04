67094f3e7eee5493791b17a66f75e42e10e125e5
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-09-04 23:11:55 +0530

    4th Commit in 800

---
+++ 800 Rating/A_Watermelon.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class A_Watermelon{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int watermelon = sc.nextInt();
+        
+        if(watermelon % 2 == 0 && watermelon > 2) System.out.println("YES");
+        else System.out.println("NO");
+}
+}
