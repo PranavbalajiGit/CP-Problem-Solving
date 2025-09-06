3edb1b6e46705a9a9673419c4fcb21add7a11d5a
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-09-06 23:14:49 +0530

    5th commit in 800

---
+++ 800 Rating/A_Game_with_Integers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class A_Game_with_Integers{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int test = sc.nextInt();
+
+        while(test != 0)
+        {
+            int num = sc.nextInt();
+            if(num % 3 == 0)
+            {
+                System.out.println("Second");
+            }
+            else{
+                System.out.println("First");
+            }
+            test--;
+        }
+    }
+}
