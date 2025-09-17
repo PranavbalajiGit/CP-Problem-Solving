0b7b2ec22db8c89a1be2200c3c507ef1c9b951e1
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-09-17 22:54:36 +0530

    12th commit in 800

---
+++ 800 Rating/A_Goals_of_Victory.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class A_Goals_of_Victory{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        while(test != 0)
+        {
+            int size = sc.nextInt();
+
+            int[] arr = new int[size];
+
+            for(int i = 0 ; i < size - 1 ; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+
+            long sum = 0 ;
+
+            for(int i = 0 ; i < size - 1 ; i++)
+            {
+                sum += arr[i];
+            }
+
+            System.out.println(-1 * sum);
+
+            test--;
+        }
+    }
+}
