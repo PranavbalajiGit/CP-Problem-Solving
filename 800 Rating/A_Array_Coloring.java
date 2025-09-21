d570ac98ea5bb77b0b853a728a207106d191dd66
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-09-21 22:30:12 +0530

    16th commit in 800

---
+++ 800 Rating/A_Array_Coloring.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class A_Array_Coloring{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        while(test != 0)
+        {
+            int size = sc.nextInt();
+            int[] arr = new int[size];
+
+            for(int i = 0 ; i < size ; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+
+            int count_of_odd = 0;
+
+            for(int i = 0 ; i < size ; i++)
+            {
+                if(arr[i] % 2 == 1) count_of_odd++;
+            }
+            
+            if(count_of_odd % 2 == 1) System.out.println("NO");
+            else System.out.println("YES");
+            
+            test--;
+        }
+    }
+}
